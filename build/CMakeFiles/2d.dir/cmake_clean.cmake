392d7d0fa93ee7a8086e1c4e7ea081dada614b6a
Heathcliff <2826190541@qq.com>
2024-08-16 18:33:37 +0800

    refactor

---
+++ build/CMakeFiles/2d.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "2d.exe"
+  "2d.exe.manifest"
+  "2d.pdb"
+  "CMakeFiles/2d.dir/src/main.cpp.obj"
+  "CMakeFiles/2d.dir/src/main.cpp.obj.d"
+  "CMakeFiles/2d.dir/src/physics_engine.cpp.obj"
+  "CMakeFiles/2d.dir/src/physics_engine.cpp.obj.d"
+  "lib2d.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sfml_test/build/CMakeFiles/sfml_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml_test.dir/src/main.cpp.obj"
+  "CMakeFiles/sfml_test.dir/src/main.cpp.obj.d"
+  "libsfml_test.dll.a"
+  "sfml_test.exe"
+  "sfml_test.exe.manifest"
+  "sfml_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
